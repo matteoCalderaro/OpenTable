ef5868f833b13ba8ace3362032b6f0235969e44c
Matteo Calderaro <matteocalderaro@gmail.com>
2023-03-18 14:20:06 +0100

    first commit 2

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -4,7 +4,7 @@ import RestaurantNavbar from "../components/RestaurantNavbar";
 const RestaurantMenu = () => {
   return (
     <div className="bg-white w-[100%] rounded p-3 shadow">
-        <RestaurantNavbar/>
+        {/* <RestaurantNavbar/> */}
         <Menu/>
     </div>
   ) 
